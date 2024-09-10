91f0f4b5469d6fbdba1cd3500e61e76719cd68da
Fenr1er <michael-koller@outlook.com>
2024-09-10 08:56:07 +0200

    controller init

---
+++ app/Http/Controllers/RecepiesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\recepies;
+use Illuminate\Http\Request;
+
+class RecepiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(recepies $recepies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(recepies $recepies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, recepies $recepies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(recepies $recepies)
+    {
+        //
+    }
+}
