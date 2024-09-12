b560dca3f26c2d31a93f6e37cd102fc6c1e32880
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-09-12 10:34:56 +0200

    changed classname

--- database/migrations/2024_09_10_093338_create_ingredients_table.php
+++ database/migrations/2024_09_10_093338_create_ingredients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIncidencesTable extends Migration
+class CreateIngredientsTable extends Migration
 {
     /**
      * Run the migrations.
