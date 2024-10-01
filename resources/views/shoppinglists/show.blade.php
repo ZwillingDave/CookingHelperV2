d2b8ee135f5b69b4b279965c15f8ef5c64447ddb
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-01 14:43:03 +0200

    removed if statements

--- resources/views/shoppinglists/show.blade.php
+++ resources/views/shoppinglists/show.blade.php
@@ -6,19 +6,14 @@
     </x-slot>
 
     <div class="py-2">
-        @foreach ($shoppinglistItems as $shoppinglistItem)
-        @if ($shoppinglistItem->shoppinglist->user->is(Auth::user()))
-        @if ($shoppinglistItem->shoppinglist->id == $id)
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">         
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    
-                    {{ __($shoppinglistItem->product_name) }}
+        @foreach ($shoppinglistItems as $shoppinglistItem)            
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">         
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">      
+                        {{ __($shoppinglistItem->product_name) }}
+                    </div>
                 </div>
             </div>
-        </div>
-        @endif
-        @endif
         @endforeach
     </div>
 </x-app-layout>
