4e80356777c5699035038763c1ae0c985c93fb74
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-02 10:50:07 +0200

    renamed from storage to storageitems and added several fields

--- database/migrations/2024_09_10_094736_create_storage_items_table.php
+++ database/migrations/2024_09_10_094736_create_storage_items_table.php
@@ -11,11 +11,15 @@ return new class extends Migration
      */
     public function up()
 {
-    Schema::create('storage', function (Blueprint $table) {
+    Schema::create('storageitems', function (Blueprint $table) {
         $table->id(); // Primärschlüssel
+        $table->unsignedBigInteger('user_id');
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         $table->unsignedBigInteger('product_id'); // Verknüpfung zu Produkten
         $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade'); // Fremdschlüssel hinzufügen
+        $table->string('product_name'); // Name des Produkts
         $table->float('quantity'); // Menge des Produkts im Lager
+        $table->unsignedBigInteger('unit_id'); // Verknüpfung zur Einheit
         $table->timestamps(); // Erstellt created_at und updated_at
     });
 }
