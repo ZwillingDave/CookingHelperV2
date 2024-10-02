6c7d2780a97507a746b2508a8d0f0e050848ebb2
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-02 10:51:37 +0200

    changed code to show all given storageitems

--- resources/views/storage/index.blade.php
+++ resources/views/storage/index.blade.php
@@ -6,18 +6,17 @@
     </x-slot>
 
     <div class="py-2">
-        @foreach ($storages as $storage)
-            @if ($storage->user_id == Auth::user()->id)
+        @foreach ($storageItems as $storageItem)
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <x-dropdown-link :href="route('storages.show', $storage->id)">
-                            <div class="p-6 text-gray-900">
-                                {{ __('Storage: ' . $storage->name . ' (Capacity: ' . $storage->capacity . ')') }}
+                    <label class="">
+                        <input type="checkbox" name="products[]" value="{{ $storageItem->id }}" class="hidden product-checkbox">
+                        <div class="product-info cursor-pointer bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900 relative">
+                                {{ __($storageItem->product_name) }}
                             </div>
-                        </x-dropdown-link>
-                    </div>
+                        </div>
+                    </label>
                 </div>
-            @endif
-        @endforeach
+            @endforeach
     </div>
 </x-app-layout>
