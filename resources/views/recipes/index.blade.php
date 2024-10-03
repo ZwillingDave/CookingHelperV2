39e07e28d9409e19878c8796333af33cd1ece9de
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-03 14:36:28 +0200

    added flex grow class and link

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -5,13 +5,15 @@
         </h2>
     </x-slot>
 
-    <div class="py-2">
+    <div class="py-2 flex-grow">
         @foreach ($recipes as $recipe)
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <x-dropdown-link :href="route('recipes.show', $recipe->id)">
                 <div class="p-6 text-gray-900">
                     {{ __($recipe->name) }}
                 </div>
+                </x-dropdown-link>
             </div>
         </div>
         @endforeach
