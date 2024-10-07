b74e8796b612af5e3e2b3be80d9278588e788878
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-07 10:42:19 +0200

    when all items in shoppinglist are purchased then show as purchased

--- resources/views/shoppinglists/index.blade.php
+++ resources/views/shoppinglists/index.blade.php
@@ -11,12 +11,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">         
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <x-dropdown-link :href="route('shoppinglists.show', $shoppinglist->id)">
-                    <div class="p-6 text-gray-900">
-                        {{ __('Shoppinglist from ' . $shoppinglist->created_at->format('d.m.Y')) }}
-                    </div>
+                        <div class="flex">
+                            @if (!$shoppinglist['notAllPurchased'])
+                            <div class="p-6 text-gray-500 flex-1">
+                                {{ __('Shoppinglist from ' . $shoppinglist->created_at->format('d.m.Y')) }}
+                            </div>
+                            <div class="p-6 flex-1 text-gray-500 text-right">{{ __("Purchased") }}</div>
+                            @else
+                            <div class="p-6 text-gray-900 flex-1">
+                                {{ __('Shoppinglist from ' . $shoppinglist->created_at->format('d.m.Y')) }}
+                            </div>
+                            @endif
+                        </div>
                 </x-dropdown-link>
                 </div>
             </div>
+            
+            
         @endif
         @endforeach
     </div>
