a4dafa8f849ec8963c1f476fcdef230c87a1fa44
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-17 14:19:15 +0200

    added add recipe link

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -22,6 +22,17 @@
             </div>
         </div>
         @endforeach
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-2">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <x-dropdown-link :href="route('recipes.create')">
+                <div class="p-6 text-gray-900">
+                    
+                    {{ __("Add Recipe") }}<div class="imgdiv"><img src="/images/add.png" alt=""></div>
+                    
+                </div>
+                </x-dropdown-link>
+            </div>
+        </div>
         </div>
     </div>
 </x-app-layout>
@@ -40,6 +51,7 @@
         
     }
     div img{
+        min-width: 200px;
         max-width: 200px;
         object-fit: scale-down;
         border-radius: 10px;
