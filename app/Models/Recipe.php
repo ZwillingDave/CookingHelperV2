2be436516a53a64cd1f2d743e45850ea46a29290
ZwillingDave <129271339+ZwillingDave@users.noreply.github.com>
2024-10-22 15:12:58 +0200

    added fillables for recipe

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -10,7 +10,7 @@ class Recipe extends Model
     use HasFactory;
 
      // Definiert, welche Felder massenweise ausgefüllt werden dürfen
-     protected $fillable = ['description', 'instruction'];
+     protected $fillable = ['name', 'image','description', 'instruction'];
 
      // Beziehung: Ein Rezept kann viele Incidences (Zutaten) haben
      public function ingredients()
